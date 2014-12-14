aaaa469f6a03ef7040f599679982aea48ff23453
linqing-lu <lulinqing@foxmail.com>
2014-12-14 15:52:34 +0800

    first commit

---
+++ iCai/iCai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iCai
+//
+//  Created by 卢林清 on 14/12/14.
+//  Copyright (c) 2014年 卢林清. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iCai/iCai/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iCai
+//
+//  Created by 卢林清 on 14/12/14.
+//  Copyright (c) 2014年 卢林清. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
